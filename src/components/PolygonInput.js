0f81335190fbbb2fe66f2faf01bc77a0a5052ed6
yj95228 <kei02119@naver.com>
2022-02-18 17:22:15 +0900

    Edit placeholder

--- src/components/PolygonInput.js
+++ src/components/PolygonInput.js
@@ -12,7 +12,7 @@ function PolygonInput(props) {
       <textarea
         className={styles.input}
         onChange={polygonChangeHandler}
-        placeholder="GeoJSON(경도, 위도의 순서) 형식으로 쉼표로 구분하여 적어주세요&#13;&#10;.&#13;&#10;
+        placeholder="WKT(경도 위도의 순서) 형식으로 쉼표로 구분하여 적어주세요&#13;&#10;.&#13;&#10;
         ex) POLYGON((126.88629117743575 37.509014447622896, 126.89022728765188 37.50573926721649, 126.89309905739181 37.50721530335715, 126.89013312271457 37.511438814295325, 126.88629117743575 37.509014447622896))"
       />
       <button onClick={submitPolygon} className={styles.button}>
