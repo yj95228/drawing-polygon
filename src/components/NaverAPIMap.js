c47839155fc020f06eea31975aa9266b2b029076
yj95228 <kei02119@naver.com>
2022-03-16 14:21:08 +0900

    feat: add JSON transformer

--- src/components/NaverAPIMap.js
+++ src/components/NaverAPIMap.js
@@ -6,11 +6,16 @@ const {
   Polygon,
 } = require("react-naver-maps");
 
-export const NaverAPIMap = ({ polygon }) => {
-  const geoArray = polygon.map((geoPoints) => {
+export const NaverAPIMap = ({ option, polygon }) => {
+  const geoArray = option === 'WKT'
+  ? polygon.map((geoPoints) => {
     const geoPoint = geoPoints.trim().split(" ");
     return { lng: geoPoint[0], lat: geoPoint[1] };
-  });
+  })
+  : polygon.map((geoPoints) => {
+    return { lng: geoPoints[0], lat: geoPoints[1] };
+  })
+  console.log(geoArray);
   return (
     <RenderAfterNavermapsLoaded clientId={"jqe51ds7wm"}>
       <NaverMap
