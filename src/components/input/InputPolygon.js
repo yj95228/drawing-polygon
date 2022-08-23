dcc142a584d049c241cb18bb7de895b50b792b71
yj95228 <yj95228@loplat.com>
2022-08-23 17:01:36 +0900

    Refactor: Split option component

--- src/components/input/InputPolygon.js
+++ src/components/input/InputPolygon.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styles from './InputPolygon.module.css';
 import Button from '../Button';
+import Option from '../Option';
 
 function InputPolygon(props) {
   const WKT = 'WKT';
@@ -45,24 +46,16 @@ ex) {"coordinates": [[[127.021057,37.503455],[127.028214,37.488473],[127.03507,3
   return (
     <>
       <div className={styles.options}>
-        <button
-          className={
-            option === WKT ? `${styles.target} ${styles.option}` : styles.option
-          }
-          onClick={selectWKT}
-        >
-          {WKT}
-        </button>
-        <button
-          className={
-            option === JSON
-              ? `${styles.target} ${styles.option}`
-              : styles.option
-          }
-          onClick={selectJSON}
-        >
-          {JSON}
-        </button>
+        {option === WKT ? (
+          <Option onClick={selectWKT} text='WKT' target />
+        ) : (
+          <Option onClick={selectWKT} text='WKT' />
+        )}
+        {option === JSON ? (
+          <Option onClick={selectJSON} text='JSON' target />
+        ) : (
+          <Option onClick={selectJSON} text='JSON' />
+        )}
       </div>
       <div onSubmit={submitPolygon} className={styles.container}>
         {option === WKT ? (
