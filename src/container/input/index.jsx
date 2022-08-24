b360d9f3d04462e33c057a0b4a5b896c51324350
yj95228 <yj95228@loplat.com>
2022-08-24 14:54:54 +0900

    Chore: Apply zustand(state library) on input page

--- src/container/input/index.jsx
+++ src/container/input/index.jsx
@@ -1,28 +1,78 @@
-import React, { useState } from 'react';
+import React from 'react';
 //TODO: module.css에서 emotion으로 완전히 변경
 import styles from './DrawContainer.module.css';
 import DrawNaverMap from './NaverMap.jsx';
-import InputPolygon from './InputPolygon.jsx';
+import useStore from '../../store/input';
 import Nav from '../../components/Nav';
+import Button from '../../components/Button';
+import Option from '../../components/Option';
+
 
 function DrawContainer() {
-  // TODO: state들 상태관리 라이브러리로 분리
-  const [polygon, setPolygon] = useState([]);
-  const [option, setOption] = useState('WKT');
-  // eslint-disable-next-line no-useless-escape
-  const regex = /[^0-9\.\s,]/g;
-  const getPolygon = (option, polygon) => {
-    setOption(option);
-    if (option === 'WKT') {
-      setPolygon(polygon.replace(regex, '').split(','));
-    } else {
-      if (polygon) setPolygon(...JSON.parse(polygon).coordinates);
+  const [WKT, JSON] = ['WKT','JSON'];
+  const WKTPlaceholder = `폴리곤을 WKT 형식으로 작성해주세요
+  
+  ex) POLYGON((126.88629117743575 37.509014447622896, 126.89022728765188 37.50573926721649, 126.89309905739181 37.50721530335715, 126.89013312271457 37.511438814295325, 126.88629117743575 37.509014447622896))`;
+  const JSONPlaceholder = `폴리곤을 JSON 형식으로 작성해주세요
+  
+  ex) {"coordinates": [[[127.021057,37.503455],[127.028214,37.488473],[127.03507,37.490567],[127.02926,37.502553],[127.028342,37.502257],[127.027602,37.502683],[127.026443,37.505066],[127.021057,37.503455]]],"type": "Polygon"}`;
+  const { input, option, polygon, selectWKT, selectJSON, setInput, drawPolygon, resetPolygon } = useStore((state) => state)
+  const onClickWKT = () => {
+    selectWKT();
+    resetPolygon(WKT)
+  }
+  const onClickJSON = () => {
+    selectJSON();
+    resetPolygon(JSON)
+  }
+  const submitPolygon = (event) => {
+    event.preventDefault();
+    drawPolygon(option, input);
+  };
+  const keyDownPolygon = (event) => {
+    setInput(event);
+    if (event.key === 'Enter') {
+      submitPolygon(event);
     }
   };
   return (
     <div className={styles.container}>
       <Nav />
-      <InputPolygon getPolygon={getPolygon} />
+      <div className={styles.options}>
+        {option === WKT ? (
+          <Option onClick={onClickWKT} text='WKT' target='true' />
+        ) : (
+          <Option onClick={onClickWKT} text='WKT' />
+        )}
+        {option === JSON ? (
+          <Option onClick={onClickJSON} text='JSON' target='true' />
+        ) : (
+          <Option onClick={onClickJSON} text='JSON' />
+        )}
+      </div>
+      <form onSubmit={submitPolygon} className={styles.form}>
+        {option === WKT ? (
+          <textarea
+            className={styles.input}
+            onChange={setInput}
+            onKeyDown={keyDownPolygon}
+            value={input}
+            placeholder={WKTPlaceholder}
+          />
+        ) : (
+          <textarea
+            className={styles.input}
+            onChange={setInput}
+            onKeyDown={keyDownPolygon}
+            value={input}
+            placeholder={JSONPlaceholder}
+          />
+        )}
+        <div className={styles.buttons}>
+          <Button onClick={submitPolygon} text='그리기' submit />
+          <Button onClick={() => {resetPolygon(option)}} text='초기화' />
+        </div>
+      </form>
       <DrawNaverMap polygon={polygon} option={option} />
     </div>
   );
--- src/store/input/index.js
+++ src/store/input/index.js
@@ -1,7 +1,31 @@
 import create from 'zustand';
 
 export const useStore = create((set) => ({
-  //TODO: 채워넣기
+  input: '',
+  option: 'WKT',
+  polygon: [],
+  selectWKT: () => set({ option: 'WKT' }),
+  selectJSON: () => set({ option: 'JSON' }),
+  setInput: (event) => set({ input: event.target.value }),
+  drawPolygon: (option, input) => {
+    if (option === 'WKT') {
+      set({ polygon: input.replace(/[^0-9\.\s,]/g, '').split(',') });
+    } else {
+      if (input) {
+        set({ polygon: JSON.parse(input).coordinates[0] });
+      }
+    }
+  },
+  resetPolygon: (option) => {
+    if (option === 'WKT') {
+      set({ input: '', polygon: [] });
+    } else {
+      set({
+        input: '',
+        polygon: JSON.parse('{"coordinates":[[]]}').coordinates,
+      });
+    }
+  },
 }));
 
 export default useStore;
