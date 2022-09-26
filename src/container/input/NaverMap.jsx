3913bf0f1aeadeb02adec56526f5d83d4b308606
yj95228 <yj95228@loplat.com>
2022-09-26 17:41:29 +0900

    Feat: WKT input 멀티폴리곤 지원

--- src/container/input/NaverMap.jsx
+++ src/container/input/NaverMap.jsx
@@ -1,43 +1,52 @@
-import React from "react";
-
-const {
+import React from 'react';
+import {
   RenderAfterNavermapsLoaded,
   NaverMap,
   Polygon,
-} = require("react-naver-maps");
+} from 'react-naver-maps';
 
 export default function DrawNaverMap({ option, polygon }) {
-  const geoArray =
-    option === "WKT"
-      ? polygon.map((geoPoints) => {
-          const [x, y] = geoPoints.trim().split(" ");
-          return { lng: x, lat: y };
+  let polygonArray =
+    option === 'WKT'
+      ? // )[공백],[공백](
+        polygon.split(/\)\s*,\s*\(/).map((polygons) => {
+          let polygon = polygons.match(
+            // 126.5 37.5, 126.7 37.6, ...의 형태
+            /\d+\.*\d*\s+\d+\.*\d*\s*(,\s*\d+\.*\d*\d+\.*\d*\d+\.*\d*\s+\d+\.*\d*)*/
+          );
+          return polygon
+            ? // [공백],[공백]
+              polygon[0].split(/\s*,\s*/).map((point) => {
+                const [x, y] = point.split(' ');
+                return { lng: parseFloat(x), lat: parseFloat(y) };
+              })
+            : { lng: undefined, lat: undefined };
         })
-      : polygon.map((geoPoints) => {
-          return { lng: geoPoints[0], lat: geoPoints[1] };
-        });
+      : polygon.map((point) => ({ lng: point[0], lat: point[1] }));
   return (
-    <RenderAfterNavermapsLoaded clientId={"jqe51ds7wm"}>
+    <RenderAfterNavermapsLoaded clientId={'jqe51ds7wm'}>
       <NaverMap
         id='maps-examples-polygon'
         style={{
-          width: "100%",
-          height: "70vh",
-          marginLeft: "20px",
-          marginRight: "20px",
+          width: '100%',
+          height: '70vh',
+          marginLeft: '20px',
+          marginRight: '20px',
         }}
-        center={geoArray[0]}
+        center={polygonArray[0][0]}
         // zoom={15}
       >
-        <Polygon
-          paths={[geoArray]}
-          fillColor={"#ff0000"}
-          fillOpacity={0.3}
-          strokeColor={"#ff0000"}
-          strokeOpacity={0.6}
-          strokeWeight={3}
-        />
+        {polygonArray[0].length && (
+          <Polygon
+            paths={polygonArray}
+            fillColor={'#ff0000'}
+            fillOpacity={0.3}
+            strokeColor={'#ff0000'}
+            strokeOpacity={0.6}
+            strokeWeight={3}
+          />
+        )}
       </NaverMap>
     </RenderAfterNavermapsLoaded>
   );
-};
+}
